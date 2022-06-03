92d659e4d267033b53a0c654c4a0af117d2d129a
EgorDubovik <posik.web.m@gmail.com>
2022-06-03 08:38:17 -0500

    Add view job list

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-Hello, world
+<div class="main-container container-fluid">
+    <!-- PAGE-HEADER -->
+    <div class="page-header">
+        <h1 class="page-title">Jobs</h1>
+        <div>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Apps</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Jobs</li>
+            </ol>
+        </div>
+    </div>
+    <!-- PAGE-HEADER END -->
+    <!-- CONTENT -->
+    <div class="row">
+        @include('job.jobs-view')
+    </div>
+
+</div>
 
 @stop
---
+++ resources/views/job/jobs-view.blade.php
@@ -0,0 +1,25 @@
+<div class="col-md-12  col-xl-6">
+    <div class="card">
+        <div class="card-status bg-green br-te-7 br-ts-7"></div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-2">
+                    <div>
+                        <strong style="font-size: 18px">1200</strong> cb.ft.
+                    </div>
+                    <div>
+                        <strong style="font-size: 18px">$5 </strong> per. cb
+                    </div>
+                </div>
+                <div class="col-10">
+                    <div>
+                        <span style="color: #9d9d9d">From:</span> <a href="#" style="font-weight: bold"> 2901 Ridgeview Dr, Plano, TX 75025</a>
+                    </div>
+                    <div style="margin-top: 10px">
+                        <span style="color: #9d9d9d">To</span>: <a href="#" style="font-weight: bold"> 22 Ochen dr, Miami, FL 33033</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
