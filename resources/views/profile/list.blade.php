7bcd80b1fca82bf72b46ee302e4625eb1a1a1099
EgorDubovik <posik.web.m@gmail.com>
2022-06-22 08:08:38 -0500

    fixes

--- resources/views/profile/list.blade.php
+++ resources/views/profile/list.blade.php
@@ -35,11 +35,10 @@
                                     <td style="text-align: center"><img src="{{ URL::asset('assets/images/users/21.jpg')}}" alt="profile-user" class="avatar-lg  profile-user brround cover-image"></td>
                                     <td class="align-middle"><a style="font-size: 18px;" href="/profile/view/{{$user->id}}">{{$user->company_name}}</a></td>
                                     <td class="align-middle">
-                                        <div class="row">
-
-                                                <div class="rating-stars block my-rating-7"  data-rating="{{$user->rating->avg('star')}}">
-                                                </div> {{$user->rating->avg('star')}}
-
+                                        <div style="color: #9d9d9d;" class="mb-2">
+                                            <span class="rating-stars my-rating-7"  data-rating="{{$user->rating->avg('star')}}"></span>
+                                            <span style="margin-left: 10px; font-weight: bold">{{$user->rating->avg('star')}}</span>
+                                            <span style="margin-left: 20px;">{{$user->rating->count()}} Reviews</span>
                                         </div>
 
                                     </td>
