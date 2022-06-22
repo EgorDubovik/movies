10450122d723dbad0aadb3bee1aab046471890c1
EgorDubovik <posik.web.m@gmail.com>
2022-06-22 07:51:41 -0500

    add review from usaer page

--- resources/views/profile/list.blade.php
+++ resources/views/profile/list.blade.php
@@ -34,11 +34,16 @@
                                 <tr>
                                     <td style="text-align: center"><img src="{{ URL::asset('assets/images/users/21.jpg')}}" alt="profile-user" class="avatar-lg  profile-user brround cover-image"></td>
                                     <td class="align-middle"><a style="font-size: 18px;" href="/profile/view/{{$user->id}}">{{$user->company_name}}</a></td>
-                                    <td>
-                                        <div class="rating-stars block my-rating-7"  data-rating="{{$user->rating->avg('star')}}">
+                                    <td class="align-middle">
+                                        <div class="row">
+
+                                                <div class="rating-stars block my-rating-7"  data-rating="{{$user->rating->avg('star')}}">
+                                                </div> {{$user->rating->avg('star')}}
+
                                         </div>
+
                                     </td>
-                                    <td>{{$user->orders->count()}}</td>
+                                    <td class="align-middle">{{$user->orders->count()}}</td>
                                     <td>{{$user->created_at}}</td>
                                 </tr>
                             @endforeach
