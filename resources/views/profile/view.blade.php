80a1c795269ff7cde9f50a2651a9948582c207a4
EgorDubovik <posik.web.m@gmail.com>
2022-07-10 10:16:29 -0500

    view favorite button on company name

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -32,40 +32,30 @@
                             </div>
                             <div class="main-chat-msg-name">
                                 <a href="#">
-                                    <h5 class="mb-1 text-dark fw-semibold">{{Auth::user()->company_name}}</h5>
+                                    <h5 class="mb-1 text-dark fw-semibold">{{$user->company_name}}</h5>
                                 </a>
-                                <p class="text-muted mt-0 mb-0 pt-0 fs-13">{{(Auth::user()->is_mover) ? "Mover" : ""}} {{(Auth::user()->is_driver) ? "Driver" : ""}}</p>
+                                <p class="text-muted mt-0 mb-0 pt-0 fs-13">{{($user->is_mover) ? "Mover" : ""}} {{($user->is_driver) ? "Driver" : ""}}</p>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="card">
-                    <div class="card-header">
-                        <div class="card-title">Reviews </div>
-                    </div>
+                    <div class="card-header"><h3 class="card-title">Favorite</h3></div>
                     <div class="card-body">
-                        @can('send-rating', $user->id)
-                            <button type="button" class="btn btn-green" data-bs-toggle="modal" data-bs-target="#input-modal" >You can leave review</button>
-                        @endcan
-                        <div style="color: #9d9d9d;" class="mb-2">
-                            <span class="rating-stars my-rating-8" data-rating="{{$user->rating->avg('star')}}"></span>
-                            <span style="margin-left: 10px; font-weight: bold">{{$user->rating->avg('star')}}</span>
-                            <span style="margin-left: 20px;">{{$user->rating->count()}} Reviews</span>
-                        </div>
-                        <table class="table text-nowrap text-md-nowrap mb-0">
-                            @foreach($user->rating as $review)
-                                <tr>
-                                    <td><a href="/profile/view/{{$review->sender->id}}">{{$review->sender->company_name}}</a></td>
-                                    <td>{{\Carbon\Carbon::parse($review->created_at)->diffForHumans()}}</td>
-                                    <td>{{$review->comment}}</td>
-                                    <td>{{$review->star}}</td>
-
-                                </tr>
-                            @endforeach
-                        </table>
+                        @if(Gate::check('add-to-favorite', $user))
+                            <div class="text-center">
+                                <a href="/favorite/add/{{$user->id}}" class="btn-success btn"><i class="fa fa-plus"></i> add to favorite</a>
+                            </div>
+                        @else
+                            <div class="text-center">
+                                <p>In your favorite list</p>
+                                <a href="/favorite/remove/{{$user->id}}" class="btn-danger btn">remove from favorite</a>
+                            </div>
+                        @endif
                     </div>
                 </div>
+
                 <div class="card panel-theme">
                     <div class="card-header">
                         <div class="float-start">
@@ -99,7 +89,32 @@
             </div>
             <div class="col-xl-8">
                 <div class="card">
+                    <div class="card-header">
+                        <div class="card-title">Reviews </div>
+                    </div>
+                    <div class="card-body">
+                        @can('send-rating', $user->id)
+                            <button type="button" class="btn btn-green" data-bs-toggle="modal" data-bs-target="#input-modal" >You can leave review</button>
+                        @endcan
+                        <div style="color: #9d9d9d;" class="mb-2">
+                            <span class="rating-stars my-rating-8" data-rating="{{$user->rating->avg('star')}}"></span>
+                            <span style="margin-left: 10px; font-weight: bold">{{$user->rating->avg('star')}}</span>
+                            <span style="margin-left: 20px;">{{$user->rating->count()}} Reviews</span>
+                        </div>
+                        <table class="table text-nowrap text-md-nowrap mb-0">
+                            @foreach($user->rating as $review)
+                                <tr>
+                                    <td><a href="/profile/view/{{$review->sender->id}}">{{$review->sender->company_name}}</a></td>
+                                    <td>{{\Carbon\Carbon::parse($review->created_at)->diffForHumans()}}</td>
+                                    <td>{{$review->comment}}</td>
+                                    <td>{{$review->star}}</td>
 
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+                <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Orders</h3>
                     </div>
